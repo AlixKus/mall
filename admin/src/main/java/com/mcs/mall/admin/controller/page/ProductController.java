b5f82ebd71d88cc7f0676073218870fbf09e2efb
mcs <787632628@qq.com>
2019-03-15 23:13:10 +0800

    ProductController

---
+++ admin/src/main/java/com/mcs/mall/admin/controller/page/ProductController.java
@@ -0,0 +1,16 @@
+package com.mcs.mall.admin.controller.page;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Api(tags = "商品管理", description = "商品管理页面")
+@RequestMapping("/product")
+public class ProductController {
+    @GetMapping("add")
+    public String addProduct() {
+        return "product/add";
+    }
+}
